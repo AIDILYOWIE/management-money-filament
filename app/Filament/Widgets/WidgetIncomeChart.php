41969f3f48e11a417ba44e9ebb5cd82322049229
aidil <yojesjasteb@gmail.com>
2025-03-20 17:13:57 +0700

    Add fitur filter by date (masih bug di chart dan line)

--- app/Filament/Widgets/WidgetIncomeChart.php
+++ app/Filament/Widgets/WidgetIncomeChart.php
@@ -6,34 +6,50 @@ use App\Models\Transaction;
 use Filament\Widgets\ChartWidget;
 use Flowframe\Trend\Trend;
 use Flowframe\Trend\TrendValue;
+use Illuminate\Support\Carbon;
+use Filament\Widgets\Concerns\InteractsWithPageFilters;
 
 class WidgetIncomeChart extends ChartWidget
 {
+    use InteractsWithPageFilters;
     protected static ?string $heading = 'Pemasukan';
     protected static string $color = 'success';
 
     protected function getData(): array
     {
+
+        $startDate = !is_null($this->filters['startDate'] ?? null) ?
+            Carbon::parse($this->filters['startDate']) :
+            null;
+
+        $endDate = !is_null($this->filters['endDate'] ?? null) ?
+            Carbon::parse($this->filters['endDate']) :
+            now();
+
+
+
         $trend = Trend::query(Transaction::incomes())
-        ->between(
-            start: now()->startOfYear(),
-            end: now()->endOfYear(),
-        )
-        ->perDay()
-        ->sum('amount');
+            ->between(
+                start: $startDate,
+                end: $endDate,
+            )
+            ->perDay()
+            ->sum('amount');
+
+
         return [
             'datasets' => [
                 [
-                    'label' => 'Pengeluaran Per Hari',
-                    'data' => $trend->map(fn (TrendValue $value) => $value->aggregate),
+                    'label' => 'Pemasukan Per Hari',
+                    'data' => $trend->map(fn(TrendValue $value) => $value->aggregate),
                 ],
             ],
-            'labels' => $trend->map(fn (TrendValue $value) => $value->date),
+            'labels' => $trend->map(fn(TrendValue $value) => $value->date),
         ];
     }
 
     protected function getType(): string
     {
-        return 'line';
+        return 'pie';
     }
 }
